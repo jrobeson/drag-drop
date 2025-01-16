7c4155a641c9f36aff589462c277da1081f1dbe9
Jon Robeson <jcbrobeson@gmail.com>
2025-01-16 14:15:31 -0600

    adding project list class

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ function validate(validatableInput: Validatable): boolean {
 	}
 	return isValid;
 }
+/////
 
 ///autobind decorator
 function autobind(_: any, _2: string, desc: PropertyDescriptor) {
@@ -41,6 +42,33 @@ function autobind(_: any, _2: string, desc: PropertyDescriptor) {
 	return adjDesc;
 }
 ///
+///// Project list
+class ProjectList {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLElement;
+	constructor(private type: 'active' | 'finished') {
+		this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLElement;
+		this.element.id = `${this.type}-projects`;
+		this.attach();
+		this.renderContent();
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector('ul')!.id = listId;
+		this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+}
+
+/////Project input aka form
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
@@ -117,3 +145,5 @@ class ProjectInput {
 }
 
 const projInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
